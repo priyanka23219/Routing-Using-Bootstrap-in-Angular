abdee7c5f9e26eed6d2d69b0b55896f725aec878
Priyanka <priyanka.23219@gmail.com>
2021-04-01 21:38:44 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,63 @@
+import {RouterModule, Routes} from '@angular/router'; 
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AboutUsComponent} from './about-us/about-us.component';
+import {AccordianComponent } from './accordian/accordian.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import {FAQComponent} from './faq/faq.component';
+import {HelpComponent} from './help/help.component';
+import { SliderComponent } from './slider/slider.component';
+import { InfoComponent} from './info/info.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"About_Us",
+    component: AboutUsComponent
+  },
+
+  {
+    path:"Contact_Us",
+    component: ContactUsComponent
+  },
+  
+  {
+    path:"Accordian",
+    component: AccordianComponent
+  },
+  {
+    path:"FAQ",
+    component: FAQComponent
+  },
+  {
+    path:"info",
+    component: InfoComponent
+  },
+  {
+    path:"slider",
+    component: SliderComponent
+  },
+  
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  
+  {
+    path:"Help",
+    component: HelpComponent
+  }
+
+
+
+];
+
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports:[RouterModule]
+  
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { SliderComponent } from './slider/slider.component';
+import { FAQComponent } from './faq/faq.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AccordianComponent } from './accordian/accordian.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent,
+    SliderComponent,
+    FAQComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    AccordianComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
